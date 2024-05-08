110c4765496e26fcd0f0a9c8dae49e50500a0227
bela1saad <belalsaad19986@gmail.com>
2024-05-08 16:14:06 +0300

    Sports fetch

--- screens/SidebarScreens/EditProfileScreen.jsx
+++ screens/SidebarScreens/EditProfileScreen.jsx
@@ -6,25 +6,37 @@ import {
   Button,
   Alert,
   Modal,
-  Dimensions,
   StyleSheet,
+  Dimensions,
+  TouchableOpacity,
 } from "react-native";
 import axiosInstance from "../../utils/axios";
 import CountryPicker, {
   CountryModalProvider,
 } from "react-native-country-picker-modal";
 import { Picker } from "@react-native-picker/picker";
+
 const { width, height } = Dimensions.get("window");
 
 const EditProfileScreen = () => {
   const [name, setName] = useState("");
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [showCountryPicker, setShowCountryPicker] = useState(false);
   const [sports, setSports] = useState([]);
   const [chosenSport, setChosenSport] = useState("");
-  const [chosenSportId, setChosenSportId] = useState(""); // New state to store the sport ID
+  const [chosenSportId, setChosenSportId] = useState(null);
+
   const [loading, setLoading] = useState(false);
   const [playerId, setPlayerId] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [positions, setPositions] = useState([]);
+  const [chosenPosition, setChosenPosition] = useState("");
+  const [chosenPositionId, setChosenPositionId] = useState(null);
+
+  const handleCountrySelect = (countryName) => {
+    setSelectedCountry(countryName);
+    setShowCountryPicker(false); // Close the country picker after selection
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,6 +45,10 @@ const EditProfileScreen = () => {
         const sportResponse = await axiosInstance.get("/sport/all");
         setSports(sportResponse.data);
 
+        // Fetch positions data from the backend
+        const positionResponse = await axiosInstance.get("/position/all");
+        setPositions(positionResponse.data);
+
         // Fetch player data from the backend
         const playerResponse = await axiosInstance.get("/player");
         const playerData = playerResponse.data;
@@ -40,9 +56,9 @@ const EditProfileScreen = () => {
           setPlayerId(playerData.id);
           setName(playerData.name);
           setSelectedCountry(playerData.location);
-          // Set the chosen sport and its ID from playerData
           setChosenSport(playerData.sport);
           setChosenSportId(playerData.sport_id);
+          setChosenPosition(playerData.position); // If position data is available
         } else {
           console.log("Player account does not exist");
         }
@@ -54,27 +70,29 @@ const EditProfileScreen = () => {
     fetchData();
   }, []);
 
-  const handleSelectSport = (sport) => {
-    // Set the chosen sport and its ID
-    setChosenSport(sport.name);
-    setChosenSportId(sport.id);
-    setShowModal(false);
+  const handleSelectSport = (sportId) => {
+    const selectedSport = sports.find(
+      (sport) => sport.id === parseInt(sportId, 10)
+    );
+    if (selectedSport) {
+      setChosenSport(selectedSport);
+      setChosenSportId(selectedSport.id);
+      setShowModal(false);
+    } else {
+      console.log("Sport not found");
+    }
   };
 
   const handleSubmit = async () => {
     try {
       setLoading(true);
 
-      // Log the chosenSportId state
-      console.log("Chosen Sport ID:", chosenSportId);
-
       const requestData = {
         name: name,
         location: selectedCountry,
-        sport_id: chosenSportId, // Send the chosen sport ID instead of the name
+        sportId: chosenSportId,
+        position: chosenPosition,
       };
-
-      console.log("Data:", requestData);
 
       const endpoint = playerId
         ? `/player/update/${playerId}`
@@ -101,6 +119,7 @@ const EditProfileScreen = () => {
       );
     }
   };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Edit Profile</Text>
@@ -116,36 +135,39 @@ const EditProfileScreen = () => {
           width: 200,
         }}
       />
-      <CountryModalProvider>
-        <CountryPicker
-          countryCode={selectedCountry && selectedCountry.cca2}
-          withFlag={true}
-          withCountryNameButton
-          withFilter
-          withAlphaFilter
-          withEmoji
-          onSelect={(value) => {
-            const countryName = value.name; // Extract the country name
-            setSelectedCountry(countryName); // Pass only the country name
-          }}
-        />
-      </CountryModalProvider>
-      <TextInput
-        placeholder="Country"
-        value={selectedCountry}
-        onChangeText={setSelectedCountry}
-        style={{
-          borderWidth: 1,
-          borderColor: "gray",
-          padding: 10,
-          margin: 10,
-          width: 200,
-        }}
-      />
+      <TouchableOpacity
+        onPress={() => setShowCountryPicker(true)}
+        style={styles.countryPickerButton}
+      >
+        <Text style={styles.countryPickerText}>
+          {selectedCountry || "Select Country"}
+        </Text>
+      </TouchableOpacity>
+      {showCountryPicker && (
+        <Modal
+          visible={true}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => setShowCountryPicker(false)}
+        >
+          <View style={styles.modalContainer}>
+            <CountryModalProvider>
+              <CountryPicker
+                withFlag
+                withCountryNameButton
+                withFilter
+                withAlphaFilter
+                withEmoji
+                onSelect={(value) => handleCountrySelect(value.name)}
+              />
+            </CountryModalProvider>
+          </View>
+        </Modal>
+      )}
       <View style={{ flexDirection: "row", alignItems: "center" }}>
         <TextInput
           placeholder="Sport"
-          value={chosenSport || "Choose a sport"}
+          value={chosenSport ? chosenSport.name : "Choose a sport"}
           editable={false}
           style={{
             borderWidth: 1,
@@ -181,14 +203,14 @@ const EditProfileScreen = () => {
             }}
           >
             <Picker
-              selectedValue={chosenSportId} // Use the sport ID as the selected value
+              selectedValue={chosenSportId}
               onValueChange={(itemValue) => handleSelectSport(itemValue)}
             >
               <Picker.Item label="Select Sport" value="" />
               {sports.map((sport) => (
                 <Picker.Item
                   label={sport.name}
-                  value={sport.id} // Use the sport ID as the value
+                  value={sport.id}
                   key={sport.id}
                 />
               ))}
@@ -197,8 +219,49 @@ const EditProfileScreen = () => {
           </View>
         </View>
       </Modal>
+      {/* Position TextInput */}
+      <TextInput
+        placeholder="Position"
+        value={chosenPosition}
+        onChangeText={setChosenPosition}
+        style={{
+          borderWidth: 1,
+          borderColor: "gray",
+          padding: 10,
+          margin: 10,
+          width: 200,
+        }}
+      />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  countryPickerButton: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    margin: 10,
+    width: 200,
+    alignItems: "center",
+  },
+  countryPickerText: {
+    textAlign: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+});
 export default EditProfileScreen;
